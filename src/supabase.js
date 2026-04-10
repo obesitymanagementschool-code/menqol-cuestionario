9184e5267277b71dc9ff7c774b0db4cf4865c90b
obesitymanagementschool-code <obesitymanagementschool@gmail.com>
2026-04-10 09:11:31 +0200

    Update supabase.js
    Actualización 10-04-2026

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://unxiutvipbqsjhphvdfj.supabase.co'
-const supabaseKey = 'sb_publishable_YuiNoHq0BBYEG14Rlks3rQ_wOcK0OIW'
+const supabaseUrl = 'https://mpoynslwcyuigxrvpzqn.supabase.co'
+const supabaseKey = 'sb_publishable_XXoGWL8CplK8EXhijNt9Rg_EMD9p474'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
