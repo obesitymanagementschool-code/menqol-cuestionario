c57a1bd70862aeaa7273c6b6673196f49f0a918d
obesitymanagementschool-code <obesitymanagementschool@gmail.com>
2026-04-13 11:09:21 +0200

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://mpoynslwcyuigxrvpzqn.supabase.co'
-const supabaseKey = 'sb_publishable_XXoGWL8CplK8EXhijNt9Rg_EMD9p474'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1wb3luc2x3Y3l1aWd4cnZwenFuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU3NzI3OTYsImV4cCI6MjA5MTM0ODc5Nn0.UXAIAk0auJT-PX94az78LVj3gJfxQk6GZG5WIqXV3cU'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
